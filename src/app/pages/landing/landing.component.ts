8da34427ebac7bbbdd7a8f0543218a99902f1116
ibtissam EL HANI <ibtissamelhani36@gmail.com>
2025-03-06 11:13:46 +0100

    first commit

---
+++ src/app/pages/landing/landing.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {NavbarComponent} from '../../shared/navbar/navbar.component';
+
+@Component({
+  selector: 'app-landing',
+  imports: [
+    NavbarComponent
+  ],
+  templateUrl: './landing.component.html',
+  styles: ``
+})
+export class LandingComponent {
+
+}
---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  template: ``,
+  styles: ``
+})
+export class NavbarComponent {
+
+}
